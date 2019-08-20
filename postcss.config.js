3cd7ed7550ed96cae453f81fcad8a04e52775b64
wangqianggithub <978159754@qq.com>
2019-08-20 23:23:24 +0800

    20190820 -night

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+//给样式加前缀，如：-webkit-tranform
+
+module.exports = {
+    plugins:[
+        require('autoprefixer')
+    ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
 //webpack  是node写出来的 node语法
 let path = require('path');
 let HtmlWebpackPlugin = require("html-webpack-plugin"); //html模板
+let MiniCssExtractPlugin = require('mini-css-extract-plugin'); //抽离<style>，形成link形式
+let OptimizeCss = require('optimize-css-assets-webpack-plugin');    //压缩css
+let UglifyJsPlugin = require('uglifyjs-webpack-plugin');    //用OptimizeCss优化css，就必须用UglifyJsPlugin优化js
 //console.log(path.resolve('dist'));
 module.exports = {
+    optimization:{  //优化项
+        minimizer:[
+            new UglifyJsPlugin({
+                cache:true,     //缓存
+                parallel:true,  //并发
+                sourceMap:true,     //是否要源码映射
+            }),
+            new OptimizeCss()
+        ]
+    },
     devServer:{ //开发服务器的配置
         port:3000,      //配置端口号
         progress:true,  //打包时候是否有进度条
         contentBase:'./build',  //启动文件的路径
         compress:true,      //是否优化
     },
-    mode:'development', //模式 development 开发    production 生产
+    mode:'production', //模式 development 开发    production 生产
     entry:'./src/index.js', //入口
     output:{
         filename:'bundle.js',   //打包后的文件名
@@ -24,6 +37,9 @@ module.exports = {
                 collapseWhitespace:true,        //压缩成一行
             },
             hash:true,      //加上hash戳,作用及时清除缓存
+        }),
+        new MiniCssExtractPlugin({
+            filename:'main.css'
         })
     ],
     module:{    //模块
@@ -35,26 +51,30 @@ module.exports = {
             {
                 test:/\.css$/,
                 use:[
-                    {
+                    /*{
                         loader:'style-loader',
                         options:{
                             insert:'top', //把<style>标签插到前面
                         }
-                    },
-                    'css-loader'
+                    },*/
+                    MiniCssExtractPlugin.loader,    //创建个link标签，把内容放进去
+                    'css-loader',
+                    'postcss-loader'   //样式前加前缀，如-webkit-
                 ]
             },
             //less-loader
             {
                 test:/\.less$/,
                 use:[
-                    {
+                    /*{
                         loader:'style-loader',
                         options:{
                             insert:'top', //把<style>标签插到前面
                         }
-                    },
+                    },*/
+                    MiniCssExtractPlugin.loader,    //创建个link标签，把内容放进去
                     'css-loader',
+                    'postcss-loader',   //样式前加前缀，如-webkit-
                     'less-loader',  //把less---css
                 ]
             }
